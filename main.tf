0e5d4a2128115e89aaffa689a9cd6e5320987fa1
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-17 13:58:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
 terraform {
-  required_version = "=1.0.6"
+  required_version = "~> 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.5.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
     }
   }
 }
