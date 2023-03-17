824c7abd5148d40595602415d2ace69b00a4c219
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-17 15:45:14 +0530

    update the shell script

---
+++ nginx/update-ecr.sh
@@ -0,0 +1,14 @@
+IMAGE = Hello
+#AWS_PROFILE = registry
+AWS_REGION = us-east-1
+AWS_ACCOUNT_ID = 932999788441
+AWS_SERVER = $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
+TAG_LATEST = $(AWS_SERVER)/$(IMAGE):latest
+
+#build:
+docker build --cache-from $(TAG_LATEST) -t $(TAG_LATEST) .
+#login:
+aws ecr get-login-password --region $(AWS_REGION)  | \
+docker login --username AWS --password-stdin $(AWS_SERVER)
+#push: build loging
+docker push $(TAG_LATEST)
