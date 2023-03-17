e14d1b731c68fd2c9359b26e7cf828c2c49279e7
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-17 17:59:27 +0530

    Update update-ecr.sh

--- nginx/update-ecr.sh
+++ nginx/update-ecr.sh
@@ -2,18 +2,13 @@
 REPO_NAME="docker_ecr_repo"
 ECR_ACCOUNT="932999788441.dkr.ecr.eu-east-1.amazonaws.com"
 ECR_REPO="${ECR_ACCOUNT}/${REPO_NAME}"
-docker build --cache-from "${ECR_REPO}" -t "${ECR_REPO}" .
+TAG="docker_ecr_repo"
+
 #aws ecr get-login-password --region us-east-1  | sed -e 's/^.*-p \(.*\)\s\-\e.*$/\1/' |  docker login --password-stdin -u AWS ${ECR_REPO}
 
 aws ecr get-login-password --region us-east-1  | sed -e 's/^.*-p \(.*\)\s\-\e.*$/\1/' |  docker login --password-stdin -u AWS 932999788441.dkr.ecr.us-east-1.amazonaws.com/docker_ecr_repo
-
-
+docker build -t "${TAG}" .
+docker tag "${TAG}":latest 932999788441.dkr.ecr.us-east-1.amazonaws.com/docker_ecr_repo:latest
 # Get the image id of the Docker build. If there is a "latest" use that else get the first without latest
-
-TAG=`docker images | grep -w "${REPO_NAME}" | grep latest | awk '{ print $3; }'`
-if [[ -z ${TAG} ]]; then
-        TAG=`docker images | grep -w "${REPO_NAME}" | awk '{ print $3; }'`
-fi
- 
-docker tag "$TAG" "${ECR_REPO}"
+docker tag "${TAG}" "${ECR_REPO}"
 docker push "${ECR_REPO}"
