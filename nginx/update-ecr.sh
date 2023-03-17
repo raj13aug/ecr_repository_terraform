0a0037b1b1f61067c28b8234113ca325727fb6e3
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-17 18:20:13 +0530

    Update update-ecr.sh

--- nginx/update-ecr.sh
+++ nginx/update-ecr.sh
@@ -3,9 +3,11 @@ REPO_NAME="docker_ecr_repo"
 ECR_ACCOUNT="932999788441.dkr.ecr.us-east-1.amazonaws.com"
 ECR_REPO="${ECR_ACCOUNT}/${REPO_NAME}"
 TAG="docker_ecr_repo"
-#
+#Login to the Docker registry on ECR.
 aws ecr get-login-password --region us-east-1  | sed -e 's/^.*-p \(.*\)\s\-\e.*$/\1/' |  docker login --password-stdin -u AWS "${ECR_REPO}"
+#Build the image
 docker build -t "${TAG}" .
+# Push using the commit tag.
 docker tag "${TAG}:latest" "${ECR_REPO}:latest"
-# Get the image id of the Docker build. If there is a "latest" use that else get the first without latest
+#Push using the latest tag.
 docker push "${ECR_REPO}:latest"
