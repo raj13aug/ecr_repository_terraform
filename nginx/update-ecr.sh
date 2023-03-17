ec2737379a021c939625f7b68671df627849f87f
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-17 17:30:44 +0530

    Update update-ecr.sh

--- nginx/update-ecr.sh
+++ nginx/update-ecr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REPO_NAME="docker_ecr_repo"
 ECR_ACCOUNT="932999788441.dkr.ecr.eu-east-1.amazonaws.com"
-ECR_REPO="${ECR_ACCOUNT}/{REPO_NAME}"
+ECR_REPO="${ECR_ACCOUNT}/${REPO_NAME}"
 docker build --cache-from "${ECR_REPO}" -t "${ECR_REPO}" .
 aws ecr get-login --region eu-east-1  | sed -e 's/^.*-p \(.*\)\s\-\e.*$/\1/' |  docker login --password-stdin -u AWS ${ECR_ACCOUNT}
 
