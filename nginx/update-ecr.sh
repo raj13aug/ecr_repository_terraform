0550bf684933be59e10e65f89f913e917c971c86
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-17 18:13:31 +0530

    Update update-ecr.sh

--- nginx/update-ecr.sh
+++ nginx/update-ecr.sh
@@ -3,11 +3,9 @@ REPO_NAME="docker_ecr_repo"
 ECR_ACCOUNT="932999788441.dkr.ecr.us-east-1.amazonaws.com"
 ECR_REPO="${ECR_ACCOUNT}/${REPO_NAME}"
 TAG="docker_ecr_repo"
-
-#aws ecr get-login-password --region us-east-1  | sed -e 's/^.*-p \(.*\)\s\-\e.*$/\1/' |  docker login --password-stdin -u AWS ${ECR_REPO}
-
-aws ecr get-login-password --region us-east-1  | sed -e 's/^.*-p \(.*\)\s\-\e.*$/\1/' |  docker login --password-stdin -u AWS 932999788441.dkr.ecr.us-east-1.amazonaws.com/docker_ecr_repo
+#
+aws ecr get-login-password --region us-east-1  | sed -e 's/^.*-p \(.*\)\s\-\e.*$/\1/' |  docker login --password-stdin -u AWS "${ECR_REPO}"
 docker build -t "${TAG}" .
-docker tag "${TAG}:latest" 932999788441.dkr.ecr.us-east-1.amazonaws.com/docker_ecr_repo:latest
+docker tag "${TAG}:latest" "${ECR_REPO}:latest"
 # Get the image id of the Docker build. If there is a "latest" use that else get the first without latest
 docker push "${ECR_REPO}:latest"
